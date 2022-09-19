5c9496b9dfc952fb0a13757232c42342fd754ca2
sthomxt <sthomxt@gmail.com>
2022-09-19 08:02:08 -0400

    minor

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
     $( "#main_form" ).submit(function(e) {
 
+        // prevent default form post
         e.preventDefault();
 
+        // store form data for ajax
         var formData = $( "#main_form" ).serializeArray();
         
         $.ajaxSetup({
@@ -17,23 +19,29 @@ $(document).ready(function() {
             data: formData,
             dataType: 'json',
             beforeSend: function(){
+                // clear fields on submit
                 $( "#msgbox" ).empty();
                 $( "#number_display" ).empty();
                 $( "#border" ).removeClass( "visible" ).addClass( "invisible" );
             },
             success: function(result){
                 $( "#msgbox" ).html(result.msg);
+                $( "#position" ).val('');
                 if (result.status=='sucess') {
                     $( "#border" ).removeClass( "invisible" ).addClass( "visible" );
                     $( "#number_display" ).html(result.num);
-                }  
+                }
             },
             error: function(data){
                 $( "#msgbox" ).html('Ajax error');
+                // clear position on error
+                $( "#position" ).val(''); 
             },
             statusCode: {
                 500: function() {
                     $( "#msgbox" ).html('Unknown Error');
+                     // clear position on error
+                    $( "#position" ).val(''); 
                 }
             },
         });
